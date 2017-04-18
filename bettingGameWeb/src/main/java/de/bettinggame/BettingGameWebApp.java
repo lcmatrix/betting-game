7e4ef19d5cb711cf6f7a18b2390c6554038f035b
Norman <norman.seidel@gmail.com>
2017-04-18 08:00:38 +0200

    Remove @EnableWebMvc annotation

--- bettingGameWeb/src/main/java/de/bettinggame/BettingGameWebApp.java
+++ bettingGameWeb/src/main/java/de/bettinggame/BettingGameWebApp.java
@@ -8,21 +8,16 @@
 
 package de.bettinggame;
 
-import org.springframework.beans.BeansException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Application starter and configuration.
  */
 @SpringBootApplication
-@EnableWebMvc
+// @EnableWebMvc
 public class BettingGameWebApp extends WebMvcConfigurerAdapter {
-    private ApplicationContext applicationContext;
 
     public static void main(String[] args) {
         SpringApplication.run(BettingGameWebApp.class, args);
