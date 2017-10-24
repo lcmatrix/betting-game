b0f600690b0d286b48be9f3f360a68495e165fc1
Norman <norman.seidel@gmail.com>
2017-10-24 23:18:29 +0200

    add password to user entity

--- model/src/main/java/de/bettinggame/model/User.java
+++ model/src/main/java/de/bettinggame/model/User.java
@@ -13,13 +13,17 @@ public class User extends AbstractIdEntity {
     private String username;
 
     @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
     private String email;
 
     public User() {
     }
 
-    public User(String username, String email) {
+    public User(String username, String password, String email) {
         this.username = username;
+        this.password = password;
         this.email = email;
     }
 
--- model/src/main/resources/db/migration/V1__initial_schema.sql
+++ model/src/main/resources/db/migration/V1__initial_schema.sql
@@ -1,8 +1,10 @@
 CREATE TABLE users (
     id INTEGER NOT NULL AUTO_INCREMENT,
     username VARCHAR(150) NOT NULL,
+    password VARCHAR(200) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    CONSTRAINT pk_user PRIMARY KEY (id)
+    CONSTRAINT pk_user PRIMARY KEY (id),
+    CONSTRAINT UNIQUE INDEX idx_user_email (email)
 );
 
 CREATE TABLE teams (
