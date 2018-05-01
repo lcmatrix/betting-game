6d21a90807446298310f5cd0009b62ab68af11da
Norman <norman.seidel@gmail.com>
2018-05-01 15:42:22 +0200

    Add message keys to TournamentLevel class

--- model/src/main/java/de/bettinggame/model/TournamentLevel.java
+++ model/src/main/java/de/bettinggame/model/TournamentLevel.java
@@ -6,10 +6,20 @@ package de.bettinggame.model;
  * @author norman
  */
 public enum TournamentLevel {
-    PRELIMINARY,
-    EIGTH_FINAL,
-    QUARTER_FINAL,
-    SEMI_FINAL,
-    SMALL_FINAL,
-    FINAL
+    PRELIMINARY("tournament.level.preliminary"),
+    EIGTH_FINAL("tournament.level.eigth_final"),
+    QUARTER_FINAL("tournament.level.quarter_final"),
+    SEMI_FINAL("tournament.level.semi_final"),
+    SMALL_FINAL("tournament.level.small_final"),
+    FINAL("tournament.level.final");
+
+    private String messageKey;
+
+    TournamentLevel(String messageKey) {
+        this.messageKey = messageKey;
+    }
+
+    public String getMessageKey() {
+        return messageKey;
+    }
 }
