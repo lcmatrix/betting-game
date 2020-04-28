9b9775bb495e0d7585efdba177307bffd4372364
Norman Seidel <norman.seidel@gmail.com>
2020-04-28 12:46:16 +0200

    Rewrite basic domain classes in Kotlin

---
+++ src/main/kotlin/de/bettinggame/domain/BaseDomainClasses.kt
@@ -0,0 +1,32 @@
+package de.bettinggame.domain
+
+import java.util.*
+import javax.persistence.Embeddable
+
+/**
+ * Instances of this interface have a message key for their representation.
+ */
+interface MessageKeyAware {
+    /**
+     * Returns the message key for this instance.
+     * @return message key
+     */
+    fun messageKey(): String
+}
+
+/**
+ * A class to support multilingual attributes in entities.
+ */
+@Embeddable
+class Multilingual(
+        private var de: String,
+        private var en: String
+) {
+    fun getValueForLocale(locale: Locale): String {
+        if (Locale.ENGLISH.language == locale.language) {
+            return en
+        }
+        // as I'm German, so default/fallback is
+        return de
+    }
+}
