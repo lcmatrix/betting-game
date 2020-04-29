c8c9a82a65f770c6f884ffb485c5395ee9b1fd51
Norman Seidel <norman.seidel@gmail.com>
2020-04-29 12:30:34 +0200

    Nullable properties in RegisterUserCommand

--- src/main/kotlin/de/bettinggame/application/RegisterUserCommand.kt
+++ src/main/kotlin/de/bettinggame/application/RegisterUserCommand.kt
@@ -9,18 +9,16 @@ import javax.validation.constraints.Size
 /**
  * Form object for registration.
  */
-class RegisterUserCommand(
+class RegisterUserCommand {
         @NotNull
         @Size(min = 1, max = 50, message = "registration.form.username.size")
-        var username: String,
+        var username: String? = null
         @NotNull
         @Size(min = 6, message = "registration.form.password.size")
-        var password: String,
+        var password: String? = null
         @NotNull
         @Size(min = 1, max = 200, message = "registration.form.email.size")
-        var email: String
-) {
-        constructor() {}
+        var email: String? = null
 
         fun createUser(identifier: String) = User(identifier, username, email, UserStatus.PENDING, UserRole.USER)
 }
