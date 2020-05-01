5be94e02a487407e2f5656eb0084276ee59f41d9
Norman Seidel <norman.seidel@gmail.com>
2020-05-01 13:13:09 +0200

    Implement forgotten TeamServiceTest

--- src/test/kotlin/de/bettinggame/application/TeamTests.kt
+++ src/test/kotlin/de/bettinggame/application/TeamTests.kt
@@ -1,30 +1,42 @@
 package de.bettinggame.application
 
 import de.bettinggame.domain.Multilingual
-import kotlin.test.assertEquals
-import java.util.Locale
-
 import de.bettinggame.domain.TeamRepository
 import de.bettinggame.domain.team.Group
 import de.bettinggame.domain.team.Team
 import org.junit.Before
-import org.junit.Rule
+import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.Mockito
-import org.mockito.junit.MockitoJUnit
-import org.mockito.junit.MockitoRule
+import org.mockito.junit.MockitoJUnitRunner
+import java.util.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
+@RunWith(MockitoJUnitRunner::class)
 class TeamServiceTest {
-    @Rule
-    val mockitoRule: MockitoRule = MockitoJUnit.rule()
     @Mock
     lateinit var teamRepository: TeamRepository
     lateinit var teamService: TeamService
 
     @Before
     fun setUp() {
-        Mockito.doAnswer { createListOfTeams() }.`when`(teamRepository.findAllByGroupCharNotNull())
+        Mockito.`when`(teamRepository.findAllByGroupCharNotNull()).thenReturn(createListOfTeams())
         teamService = TeamService(teamRepository)
+    }
+
+    @Test
+    fun testGetAllGroupsWithTeams() {
+        val allGroupsWithTeams = teamService.getAllGroupsWithTeams()
+        assertEquals(2, allGroupsWithTeams.size, "2 groups expected")
+        val iterator = allGroupsWithTeams.iterator()
+        val groupF = iterator.next()
+        assertLocalizedTeams(Locale("de", "DE"), groupF.teams[0].name)
+        assertEquals(Group.F.name, groupF.groupChar)
+
+        val groupH = iterator.next()
+        assertLocalizedTeams(Locale("fr", "FR"), groupH.teams[0].name)
+        assertEquals(Group.H.name, groupH.groupChar)
     }
 
     private fun assertLocalizedTeams(locale: Locale, teamName: String) {
