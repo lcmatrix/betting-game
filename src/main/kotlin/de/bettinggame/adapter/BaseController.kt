b26fc68cd684de5eef0b95b814bbab434ab2dcc0
Norman <norman.seidel@gmail.com>
2020-05-04 12:31:59 +0200

    Remove @JvmDefault annotation since all controllers are written in Kotlin

--- src/main/kotlin/de/bettinggame/adapter/BaseController.kt
+++ src/main/kotlin/de/bettinggame/adapter/BaseController.kt
@@ -13,15 +13,12 @@ interface AbstractController {
     /**
      * Returns navigation items
      */
-    @JvmDefault
     @ModelAttribute("nonRestrictedNavigation")
     fun getNonRestrictedNavigation(): List<Navigation> = Navigation.getNonRestrictedNavigation()
 
-    @JvmDefault
     @ModelAttribute("userRestrictedNavigation")
     fun getUserRestrictedNavigation(): List<Navigation> = Navigation.getUserRestrictedNavigation()
 
-    @JvmDefault
     @ModelAttribute("adminRestrictedNavigation")
     fun getAdminRestrictedNavigation(): List<Navigation> = Navigation.getAdminRestrictedNavigation()
 }
